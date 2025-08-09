a0945471fae6b079966c809444d83aedaf90ee2c
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2025-08-09 07:54:20 -0700

    feat: support vectorized kernel

--- lab/vector_add/main.c
+++ lab/vector_add/main.c
@@ -15,6 +15,7 @@
 
 #define VECTOR_ADD_2_KERNEL_PATH "vector_add_2.cl"
 #define VECTOR_ADD_4_KERNEL_PATH "vector_add_4.cl"
+#define VECTOR_ADD_4_VECTORIZED_KERNEL_PATH "vector_add_4_vectorized.cl"
 
 void initializeOpenCL(cl_device_id* device_id, cl_context* context, cl_command_queue* queue) {
     cl_int err;
@@ -204,6 +205,89 @@ void part2(Matrix* host_input_1, Matrix* host_input_2, Matrix* host_input_3, Mat
     SaveMatrix(output_file, host_output);
 
     //@@ Release OpenCL objects here
+}
+
+void part3(Matrix* host_input_1, Matrix* host_input_2, Matrix* host_input_3, Matrix* host_input_4, Matrix* host_output, Matrix* answer, const char* output_file) {
+    // Start of program three
+
+    // OpenCL objects
+    cl_device_id device_id;             // device ID
+    cl_context context;                 // context
+    cl_command_queue queue;             // command queue
+
+    initializeOpenCL(&device_id, &context, &queue);
+
+    callVectorAdd4KernelVectorized(host_input_1, host_input_2, host_input_3, host_input_4, host_output, &context, &queue);
+
+    // Prints the results
+    // for (unsigned int i = 0; i < host_output.shape[0] * host_output.shape[1]; i++)
+    // {
+    //     printf("C[%u]: %d == %d\n", i, host_output.data[i], answer.data[i]);
+    // }
+
+    // Check whether the answer matches the output
+    CheckMatrix(answer, host_output);
+    SaveMatrix(output_file, host_output);
+
+    //@@ Release OpenCL objects here
+}
+
+void callVectorAdd4KernelVectorized(Matrix* a, Matrix* b, Matrix* c, Matrix* d, Matrix* out, cl_context* context, cl_command_queue* queue) {
+    // OpenCL objects
+    cl_program program;                 // program
+    cl_kernel kernel;         // kernel
+
+    // OpenCL setup variables
+    size_t global_item_size, local_item_size;
+    cl_int err;
+
+    // Device input and output vectors
+    cl_mem device_input_1, device_input_2, device_input_3, device_input_4, device_output;
+
+    // Load external OpenCL kernel code
+    char *kernel_source = OclLoadKernel(VECTOR_ADD_4_VECTORIZED_KERNEL_PATH);
+
+    // Create the program from the source buffer
+    program = clCreateProgramWithSource(*context, 1, (const char **)&kernel_source, NULL, &err);
+    CHECK_ERR(err, "clCreateProgramWithSource");
+
+    // Build the program executable
+    err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+    CHECK_ERR(err, "clBuildProgram");
+    
+    kernel = clCreateKernel(program, "vectorAdd", &err);
+    CHECK_ERR(err, "clCreateKernel");
+
+    // Allocate GPU memory
+    //@@ Create memory buffers for input and output vectors
+
+    //@@ Copy memory to the GPU here
+
+    //@@ define local and global work sizes
+    unsigned int size_a = 0; // @@ replace this with length of the input vector(s)
+
+    // Set the arguments to the kernel
+    err = clSetKernelArg(kernel, 0, sizeof(cl_mem), &device_input_1);
+    CHECK_ERR(err, "clSetKernelArg 0");
+    err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &device_input_2);
+    CHECK_ERR(err, "clSetKernelArg 1");
+    err |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &device_input_3);
+    CHECK_ERR(err, "clSetKernelArg 2");
+    err |= clSetKernelArg(kernel, 3, sizeof(cl_mem), &device_input_4);
+    CHECK_ERR(err, "clSetKernelArg 3");
+    err |= clSetKernelArg(kernel, 4, sizeof(cl_mem), &device_output);
+    CHECK_ERR(err, "clSetKernelArg 4");
+    err |= clSetKernelArg(kernel, 5, sizeof(unsigned int), &size_a);
+    CHECK_ERR(err, "clSetKernelArg 5");
+
+    //@@ Launch the GPU Kernel here
+
+    //@@ Copy the GPU memory back to the CPU here
+
+    //@@ Free the GPU memory here
+
+    // Release Host Memory
+    free(kernel_source);
 }
 
 int main(int argc, char *argv[])
